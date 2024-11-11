d1e15981a68eaa2d5bd45c32880c8bfa3039b102
codebaby <58240519+webmaxiansen@users.noreply.github.com>
2024-11-11 18:19:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def get_birthday():
 
 def get_words():
   # words = requests.get("https://api.shadiao.pro/chp")
-  words = requests.get("https://api.vvhan.com/api/text/love")
+  words = requests.get("https://api.vvhan.com/api/text/love?type=json")
   if words.status_code != 200:
     return get_words()
   # return words.json()['data']['text']
